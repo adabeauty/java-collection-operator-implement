d9ed0028d39d007ed1f8d3ccad756bdcd10080ef
hawang <654296217@qq.com>
2015-04-29 14:08:58 +0800

    implement filter class

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -2,6 +2,8 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -14,31 +16,91 @@ public class Filter {
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+
+        for(int i=0; i<array.size(); i++){
+            if(array.get(i)%2 == 0){
+                result.add(array.get(i));
+            }
+        }
+
+        return result;
     }
 
     public List<Integer> filterMultipleOfThree() {
+        List<Integer> result = new ArrayList<>();
+
+        for(int i=0; i<array.size(); i++){
+            if(array.get(i)%3 == 0){
+                result.add(array.get(i));
+            }
+        }
+
+        return result;
+    }
+
+    public List<Integer> getDifferentElements() {
+        List<Integer> result = new ArrayList<>();
+
+        for(int i=0; i<array.size(); i++){
+            if(!result.contains(array.get(i))){
+                result.add(array.get(i));
+            }
+        }
+
+        return result;
+    }
+
+    public Map<Integer, Integer> getMapOfArrayList() {
+        Map map = new HashMap<Integer,Integer>();
+
+        for(int i=0; i<array.size(); i++){
+
+            Integer element = array.get(i);
+            if(map.containsKey(element)){
+
+                Integer previousValue = (Integer) map.get(element);
+                map.put(element, ++previousValue);
+            }else{
+                map.put(element, 1);
+            }
+        }
+
+        return map;
+    }
+
+    public Integer[][] getDividedArray() {
+
+//        for(int i=0; i<array.size(); i++){
+//            for()
+//        }
+//        return null;
         throw new NotImplementedException();
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
-    }
+        List<Integer> result = new ArrayList<>();
 
-    public List<Integer> getDifferentElements() {
-        throw new NotImplementedException();
+        for(int i=0; i<firstList.size(); i++){
+            if(secondList.contains(firstList.get(i))){
+                result.add(firstList.get(i));
+            }
+        }
+
+        return result;
     }
 
     public List<Integer> getUncommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
-    }
 
-    public Map<Integer, Integer> getMapOfArrayList() {
-        throw new NotImplementedException();
-    }
+        List<Integer> result = new ArrayList<>();
 
-    public Integer[][] getDividedArray() {
-        return null;
+        for(int i=0; i<firstList.size(); i++){
+            if(!secondList.contains(firstList.get(i))){
+                result.add(firstList.get(i));
+            }
+        }
+
+        return result;
     }
 
     public List<Integer> getElementsByRelationship(FilterHandler filterHandler, List<Integer> objectList) {
