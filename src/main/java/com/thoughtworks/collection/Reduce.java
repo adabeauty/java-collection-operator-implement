91639d1226e677d0e4e574c48c6f2ada95dba423
hawang <654296217@qq.com>
2015-04-29 15:00:12 +0800

    implement mehtods of Reduce class

--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -13,39 +13,138 @@ public class Reduce {
     }
 
     public int getSum() {
-        throw new NotImplementedException();
+        int sum = 0;
+
+        for(int i=0; i<arrayList.size(); i++){
+            sum += arrayList.get(i);
+        }
+
+        return sum;
     }
 
     public int getMaximum() {
-        throw new NotImplementedException();
+        int maximum = arrayList.get(0);
+
+        for(int i=1; i<arrayList.size(); i++){
+            if(arrayList.get(i) > maximum){
+                maximum = arrayList.get(i);
+            }
+        }
+
+        return maximum;
     }
 
     public double getMinimum() {
-        throw new NotImplementedException();
+        int mimimum = arrayList.get(0);
+
+        for(int i=1; i<arrayList.size(); i++){
+            if(arrayList.get(i) < mimimum){
+                mimimum = arrayList.get(i);
+            }
+        }
+
+        return mimimum;
     }
 
     public double getOrderedMedian() {
-        throw new NotImplementedException();
+        double result;
+        int index;
+
+        if(arrayList.size()%2 != 0){
+            index = arrayList.size()/2;
+            result = arrayList.get(index);
+        }else{
+            index = arrayList.size()/2;
+            result = ((double)(arrayList.get(index) + arrayList.get(index-1)))/2;
+        }
+
+        return result;
     }
 
     public double getUnorderedMedian() {
-        throw new NotImplementedException();
-    }
 
-    public int getFirstEven() {
-        throw new NotImplementedException();
-    }
+        List<Integer> orderList = arrayList;
+        for(int i=0; i<orderList.size(); i++){
+            for(int j=i; j<orderList.size(); j++){
 
-    public int getIndexOfFirstEven() {
-        throw new NotImplementedException();
-    }
+                if(orderList.get(i) < orderList.get(j)){
+                    int temp = orderList.get(i);
+                    orderList.set(i,orderList.get(j));
+                    orderList.set(j, temp);
+                }
+            }
 
-    public boolean isEqual(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        }
+
+        Reduce reduce = new Reduce(orderList);
+        return reduce.getOrderedMedian();
     }
 
     //实现接口SingleLink，然后再此函数内使用
     public Double getMedianInLinkList() {
         throw new NotImplementedException();
+    }
+
+    public int getFirstEven() {
+
+        for(int i=0; i<arrayList.size(); i++){
+            if(arrayList.get(i)%2 == 0){
+                return arrayList.get(i);
+            }
+        }
+
+        return -1;
+    }
+
+    public int getIndexOfFirstEven() {
+        for(int i=0; i<arrayList.size(); i++){
+            if(arrayList.get(i)%2 == 0){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public boolean isEqual(List<Integer> objectList) {
+
+        if(objectList.size() != arrayList.size()){
+            return false;
+        }else{
+            for(int i=0; i<arrayList.size(); i++){
+
+                if(arrayList.get(i) != objectList.get(i)){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public int getLastEven() {
+
+        for(int i=arrayList.size()-1; i>=0; i--){
+            if(arrayList.get(i)%2 == 1){
+                return arrayList.get(i);
+            }
+        }
+        return -1;
+    }
+
+    public int getIndexOfLastEven() {
+        for(int i=arrayList.size()-1; i>=0; i--){
+            if(arrayList.get(i)%2 == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public double getAverage() {
+        Reduce reduce = new Reduce(arrayList);
+
+        double sum = (double) reduce.getSum();
+        return sum/arrayList.size();
     }
 }
